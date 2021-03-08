7c12fc594075d9c98477dff897e0a73e9d633a64
Jorge Martin <jorgemaram@gmail.com>
2021-03-08 14:04:21 +0100

    first css steps for navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,30 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import React from 'react';
+import { css } from '@emotion/react';
+import Navegacion from './nav';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+const Header = () => {
+    return (
+        <header css={css`
+            background-color: #333;
+            padding: 1rem;
+        `}>
+            <div css={css`
+                max-width: 1200px;
+                margin: 0 auto;
+                @media (min-width: 768px){
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                }
+            `}>
+                <h1 css={css`
+                    color: #FFF;
+                    text-align: center;    
+                `}>Hotel Gatsby</h1>
+                <Navegacion/>
+            </div>
+        </header>
+     );
 }
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+ 
+export default Header;
---
+++ src/pages/nosotros.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from '../components/layout';
+
+
+const IndexPage = () => {
+    return (
+        <Layout>
+            <h1>Nosotros</h1>
+        </Layout>
+    )
+}
+
+export default IndexPage
