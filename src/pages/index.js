95555b827035c25b815d46dfadd9a0225df93634
Jorge Martin <jorgemaram@gmail.com>
2021-03-09 13:39:51 +0100

    editing homepage and about us page. Starting room's pages.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,36 @@
 import React from "react";
+import { css } from '@emotion/react';
 import Layout from '../components/layout';
 import ImagenHotel from '../components/imagenHotel';
+import ContenidoInicio from '../components/contenidoInicio';
+import HabitacionPreview from '../components/habitacionPreview';
+import useHabitaciones from '../hooks/use-habitaciones';
+
+
 
 const IndexPage = () => {
+
+  const habitaciones = useHabitaciones();
+  console.log(habitaciones)
+
   return (
     <Layout>
       <ImagenHotel/>
+      <ContenidoInicio />
+      <h2 css={css`
+        text-align: center;
+        margin-top: 5rem;
+        font-size: 3rem;
+      `}>Nuestras habitaciones</h2>
+
+      <ul>
+        {habitaciones.map(habitacion => (
+          <HabitacionPreview
+            key={habitacion.id}
+            
+          />
+        ))}
+      </ul>
     </Layout>
   )
 }
